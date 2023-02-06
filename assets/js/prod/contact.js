3f8b6f2b0b4c3d7b3639f5966e0f1c01305c7db8
Brent Robbins <brent@variantstudios.com>
2023-02-06 14:19:25 -0700

    Convert contact for to Netlify form - test

--- assets/js/prod/contact.js
+++ assets/js/prod/contact.js
@@ -1,1 +1,1 @@
-function queryString(){for(var o=window.location.search.split("&"),t=0;t<o.length;t++)return o[t].split("=")[1]}$("body").hasClass("contact")&&("true"==queryString()?($("#contact-message").show(),console.log("show")):($("#contact-message").hide(),console.log("hide")));
+// function queryString(){for(var o=window.location.search.split("&"),t=0;t<o.length;t++)return o[t].split("=")[1]}$("body").hasClass("contact")&&("true"==queryString()?($("#contact-message").show(),console.log("show")):($("#contact-message").hide(),console.log("hide")));
